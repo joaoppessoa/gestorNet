b1a3939df287c686d79fe2aed554e6860d3ad0a3
joaoppessoa <joao.paulo@academico.ifpb.edu.br>
2020-03-09 16:01:54 -0300

    Definição de Rotas no Laravel.

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,69 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Esse tipo de Rota se define um methodo e só pode usar ele.
+Route::get('/estoque', function (){
+   return view("exibicao/estoque");
+});
+
+// Esse tipo de Rota pode se usar qualquer methodo do html.
+Route::any('any', function (){
+    return "any";
+});
+
+// Esse tipo de Rota pode se usar qualquer methodo definido no array que é passado no primeiro parametro.
+Route::match(['post', 'get'], 'match', function (){
+   return "match";
+});
+
+// Esse como passar vallores dinamicos na rota.
+Route::get('categoria/{var}', function ($a){
+    return "produtos da categoria: {$a}";
+});
+
+// Esse como passar vallores dinamicos na rota junto a um complemento fixo.
+Route::get('produtos/{var}/detalhes', function ($var){
+    return "detalhes de {$var}";
+});
+
+// Esse tipo de rota deixa livre para o usuario passar padrão ou não.
+Route::get('produto/{idProduto?}', function ($idProduto = ''){
+    return "Produto {$idProduto}";
+});
+
+// Exeplo 1 de redirecionamento de rotas.
+Route::get('redirect1', function (){
+    return redirect()->route('teste.name');
+});
+
+// Exeplo 2 de redirecionamento de rotas.
+Route::redirect('redirect3', 'redirect2');
+
+Route::get('redirect2', function (){
+    return "redirect2";
+})->name('teste.name');
+
+
+Route::get('login', function (){
+    return "login";
+})->name('login');
+
+Route::middleware([])->group(function (){
+    Route::prefix('admin')->group(function (){
+        Route::get('inicial', function (){
+            return "Menu";
+        });
+
+        Route::get('finaceiro', function (){
+            return "Finaceiro";
+        });
+
+        Route::get('/', function (){
+            return "Menu";
+        });
+    });
 });
